0d64b889cbdc5c6acd12ef85e518a283634a2367
Henryk <henipiter@wp.pl>
2022-03-31 21:24:05 +0200

    fix updating interval value; add pop-up window while updating interval; fix refresh calendar at start and after add/edit note

--- app/src/main/java/com/example/kaledarz/MainActivity.kt
+++ app/src/main/java/com/example/kaledarz/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.example.kaledarz
 
 
-import android.content.Context
 import android.content.Intent
-import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.CalendarView
-import android.widget.CalendarView.OnDateChangeListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,12 +21,44 @@ class MainActivity : AppCompatActivity() {
     var choose_date = "2020-01-01"
 
 
-    var _id:ArrayList<String> = ArrayList<String>()
-    var _date:ArrayList<String> = ArrayList<String>()
-    var _time:ArrayList<String> = ArrayList<String>()
-    var _interval:ArrayList<String> = ArrayList<String>()
-    var _content:ArrayList<String> = ArrayList<String>()
+    var _id: ArrayList<String> = ArrayList()
+    var _date: ArrayList<String> = ArrayList()
+    var _time: ArrayList<String> = ArrayList()
+    var _interval: ArrayList<String> = ArrayList()
+    var _content: ArrayList<String> = ArrayList()
     lateinit var customAdapter: CustomAdapter
+
+    private fun getTodayDate(): String {
+        val calender = Calendar.getInstance()
+        calender.timeInMillis = calendar.date
+        val year = calender[Calendar.YEAR].toString()
+        var month = (1 + calender[Calendar.MONTH]).toString()
+        var curDate = calender[Calendar.DAY_OF_MONTH].toString()
+
+        if (curDate.length == 1) {
+            curDate = "0$curDate"
+        }
+        if (month.length == 1) {
+            month = "0$month"
+        }
+        return "$curDate-$month-$year"
+    }
+
+    private fun getChosenDate(year: Int, month: Int, dayOfMonth: Int): String {
+        val curDate: String
+        val monthStr: String
+        if (dayOfMonth < 10) {
+            curDate = "0$dayOfMonth"
+        } else {
+            curDate = dayOfMonth.toString()
+        }
+        if (month + 1 < 10) {
+            monthStr = "0" + (month + 1).toString()
+        } else {
+            monthStr = (month + 1).toString()
+        }
+        return "$curDate-$monthStr-$year"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,72 +69,56 @@ class MainActivity : AppCompatActivity() {
         val cal = CalendarView(this)
         cal.setDate(System.currentTimeMillis(), false, true)
 
-        val date: Long = calendar.getDate()
-        val calender = Calendar.getInstance()
-        calender.timeInMillis = date
-        var Year = calender[Calendar.YEAR].toString()
-        var Month = (1+calender[Calendar.MONTH]).toString()
-        var curDate = calender[Calendar.DAY_OF_MONTH].toString()
-
-        choose_date=curDate+"-"+Month+"-"+Year
-        choose_date = "11-07-2021"
+        choose_date = getTodayDate()
         myDB = MyDatabaseHelper(this)
 
-        customAdapter = CustomAdapter(this,this,_id,_date, _time, _interval, _content)
-        recyclerViewEvent.setAdapter(customAdapter)
-        recyclerViewEvent.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL ,false)
+        customAdapter = CustomAdapter(this, this, _id, _date, _time, _interval, _content)
+        recyclerViewEvent.adapter = customAdapter
+        recyclerViewEvent.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         storeDataInArrays()
         customAdapter.notifyDataSetChanged()
 
 
 
-        calendar.setOnDateChangeListener(OnDateChangeListener { view, year, month, dayOfMonth ->
-            if(dayOfMonth<10)
-                curDate = "0"+ dayOfMonth.toString()
-            else
-                curDate = dayOfMonth.toString()
-            Year = year.toString()
-            if(month+1<10)
-                Month = "0"+ (month+1).toString()
-            else
-                Month =  (month+1).toString()
+        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
 
-            choose_date=curDate+"-"+Month+"-"+Year
+
+            choose_date = getChosenDate(year, month, dayOfMonth)
             Log.e("aa", choose_date)
             storeDataInArrays()
             customAdapter.notifyDataSetChanged()
 
-        })
-        addNew.setOnClickListener(View.OnClickListener {
+        }
+        addNew.setOnClickListener {
 
-            val i = Intent(this, com.example.kaledarz.AddEventActivity::class.java)
-            i.putExtra("year", Year)
-            i.putExtra("month", Month)
-            i.putExtra("dayOfMonth", curDate)
+            val i = Intent(this, AddEventActivity::class.java)
+            i.putExtra("date", choose_date)
             this.startActivity(i)
-        })
+        }
 
 
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==1){
+        if (requestCode == 1) {
             recreate()
         }
     }
-    fun storeDataInArrays(){
+
+    private fun storeDataInArrays() {
         _id.clear()
         _date.clear()
         _time.clear()
         _interval.clear()
         _content.clear()
         val cursor = myDB.readAllData(choose_date)
-        if(cursor?.count == 0){
+        if (cursor?.count == 0) {
             Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show()
-        }
-        else{
-            while(cursor?.moveToNext() == true){
+        } else {
+            while (cursor?.moveToNext() == true) {
                 _id.add(cursor.getString(0))
                 _date.add(cursor.getString(1))
                 _time.add(cursor.getString(2))

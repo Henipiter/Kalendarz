512a4b36587df84d2c5d423e3c2f5f5183e06df6
Henryk <henipiter@wp.pl>
2022-04-22 20:12:51 +0200

    add design for SegregatedList

---
+++ app/src/main/java/com/example/kaledarz/SegregatedList.kt
@@ -0,0 +1,11 @@
+package com.example.kaledarz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegregatedList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segregated_list)
+    }
+}
